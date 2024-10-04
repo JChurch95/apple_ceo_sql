c49b88047761a0492244093d9f12cf0aa4dde85e
Jordan A. Church <jordanchurch95@gmail.com>
2024-10-04 08:54:07 -0400

    Add files via upload
    first commit

---
+++ apple_ceos_sql/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE apple_ceos (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+    slug TEXT NOT NULL
+    first_year_active INTEGER
+);
+    
---
+++ apple_ceos_sql/seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO apple_ceos (name, year) VALUES
+('Michael Spindler', 'michael-splinder', 1993),
+('Gil Amelio', 'gil-amelio', 1996),
+('Mike Markkula', 'mike-markkula', 1977),
+('Tim Cook', 'tim-cook', 2011),
+('Mike Scott', 'mike-scott', 1978),
+('John Sculley', 'john-sculley', 1983),
+('Steve Jobs', 'steve-jobs', 1976),
+('Steve Jobs', 'steve-jobs' 1997);
